50b76d4f6c929067f244d669aaca804851467236
Roger Koche <roger@koche.com>
2012-07-27 15:02:57 -0400

    Make a basic User model (including secure passwords)

--- Gemfile
+++ Gemfile
@@ -25,6 +25,10 @@ group :test, :development do
 	gem 'spork', '0.9.0'
 end
 
+group :development do
+	gem 'annotate', '~> 2.4.1.beta'
+end
+
 group :test do
 	gem 'capybara', '1.1.2'
 	gem 'factory_girl_rails', '1.4.0'
--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,11 @@ SampleApp::Application.routes.draw do
 
   root to: 'static_pages#home'
 
-  match '/signup', to: 'users#new'
+  match '/signup' => 'users#new'
 
-  match '/help',  to: 'static_pages#help'
-  match '/about',  to: 'static_pages#about'
-  match '/contact',  to: 'static_pages#contact'
+  match '/help' => 'static_pages#help'
+  match '/about' => 'static_pages#about'
+  match '/contact' => 'static_pages#contact'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
